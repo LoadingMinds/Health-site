d160cc9b90d52b7258f17389e3ceb9c152a05f65
Indrani Som <indranisom39@gmail.com>
2023-12-30 02:55:30 -0800

    added: made the home section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["*"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'custom': ['Kanit', 'fallbackFont', 'sans-serif']
+      },
+      colors: {
+        'backgroundcolor1': 'var(--backgroundcolor1)',
+        'navcolor': 'var(--navcolor)',
+        'textcolor1': 'var(--textcolor1)',
+        'textcolor2': 'var(--textcolor2)',
+        'color3': 'var(--color3)',
+        'color1': 'var(--color1)',
+      }
+    },
   },
   plugins: [],
 }
